bc4d845a3e1601091218356d5a2afbc90ca7d3c8
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-03-11 15:36:56 -0400

    1. 暴力解 2. 哈希表两次遍历 3. 哈希表一次遍历

---
+++ src/Array/TwoSum.java
@@ -0,0 +1,57 @@
+package Array;
+
+/*  1. Two Sum
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    Example:
+    Given nums = [2, 7, 11, 15], target = 9
+    return [0, 1]
+ */
+import java.util.HashMap;
+//      One-Pass Hash Table: Time = O(n) Space = O(n)
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // While we iterate and inserting elements into the table, we also look back to check
+        // if current element's complement already exists in the table
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+/*  Two-pass Hash Table: Time = O(n) Space = O(n)
+
+        // {value, index}
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // In the first iteration, we add each element's value and its index to the table
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        // In the second iteration, we check if each element's complement (target - nums[i]) exists in the table.
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            // Beware that the complement must not be nums[i] itself!
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+ */
+
+/*  Brute Force: Time = O(n^2) Space = O(1)
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+ */
