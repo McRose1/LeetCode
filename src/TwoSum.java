b002059949a21a94d48024ed7a2c995470ae4e4e
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-03-11 14:13:43 -0400

    TwoSum.java

---
+++ src/TwoSum.java
@@ -0,0 +1,39 @@
+/*
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    Example:
+    Given nums = [2, 7, 11, 15], target = 9
+    return [0, 1]
+ */
+//      Two-pass Hash Table
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+/*
+    One-Pass Hash Table
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+ */
