563cd3d590c88c94b966195222330d7a80c2269d
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-04-25 00:48:54 -0400

    4.24

--- src/Design/LRUCache.java
+++ src/Design/LRUCache.java
@@ -1,8 +1,5 @@
 package Design;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /*  146. LRU Cache
     Design and implement a data structure for Least Recently Used (LRU) cache.
     It should support the following operations: get and put.
@@ -26,85 +23,107 @@ import java.util.Map;
     cache.get(3);        // returns 3
     cache.get(4);        // returns 4
  */
-//  HashMap + Doubly LinkedList: Time = O(1)
+import java.util.HashMap;
+import java.util.Map;
+/*  HashMap + Doubly LinkedList: Time = O(1) Space = O(capacity)
+    获取键/检查键是否存在 -> HashMap O(1)
+    设置键 -> HashMap O(1)
+    删除最先插入的键 -> Doubly LinkedList O(1)
+ */
 public class LRUCache {
-    class Node {
+    class DLinkedNode {
         int key;
         int val;
-        Node prev;
-        Node next;
-        // 构造函数
-        public Node(int k, int v) {
-            key = k;
-            val = v;
-        }
+        DLinkedNode prev;
+        DLinkedNode next;
     }
-    Node dummyHead;
-    Node dummyTail;
+
+    DLinkedNode dummyHead;
+    DLinkedNode dummyTail;
     // 记住 map 存的 value 是 Node
-    Map<Integer, Node> map;
+    Map<Integer, DLinkedNode> cache = new HashMap<>();
     int size;
     int capacity;
 
+    // Always add the new node to the head of doubly LinkedList.
+    private void addNode(DLinkedNode node) {
+        // dummyHead <- node
+        node.prev = dummyHead;
+        // node -> formerHead
+        node.next = dummyHead.next;
+
+        // node <- formerHead
+        dummyHead.next.prev = node;
+        // dummyHead -> node
+        dummyHead.next = node;
+    }
+
+    // Remove an existing node form the tail of doubly LinkedList.
+    private void removeNode(DLinkedNode node) {
+        DLinkedNode prev = node.prev;
+        DLinkedNode next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    // Move certain node in between to the head of doubly LikedList.
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    // Pop the current tail
+    private DLinkedNode popTail() {
+        DLinkedNode res = dummyTail.prev;
+        removeNode(res);
+        return res;
+    }
+
     public LRUCache(int capacity) {
-        dummyHead = new Node(0, 0);
-        dummyTail = new Node(0, 0);
+        dummyHead = new DLinkedNode();
+        dummyTail = new DLinkedNode();
         // 初始化 Doubly LinkedList
         dummyHead.next = dummyTail;
         dummyTail.prev = dummyHead;
 
-        map = new HashMap<>();
         size = 0;
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        if (!map.containsKey(key)) {
+        if (!cache.containsKey(key)) {
             return -1;
         } else {
+            DLinkedNode node = cache.get(key);
             // hit 就把该 node 移到双向链表的头部
-            Node node = map.get(key);
-            remove(key);
-            addHead(key, node.val);
+            moveToHead(node);
             return node.val;
         }
     }
 
     public void put(int key, int value) {
-        // 修改原有的值，在这里体现为先删去 node，在把带有新 value 的 node 加到头部
-        if (map.containsKey(key)) {
-            remove(key);
-            addHead(key, value);
+        // 修改原有的值，再把带有新 value 的 node 移到头部
+        if (cache.containsKey(key)) {
+            DLinkedNode node = cache.get(key);
+            node.val = value;
+            moveToHead(node);
         // 直接在头部加一个新的 node
         } else {
-            addHead(key, value);
-        }
-    }
-    // remove from tail
-    private void remove(int key) {
-        Node cur = map.get(key);
-        Node next = cur.next;
-        Node prev = cur.prev;       // prev <-> cur <-> next
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.val = value;
 
-        prev.next = next;           // prev -> next
-        next.prev = prev;           // prev <- next
-        size--;
-        map.remove(key);
-    }
+            cache.put(key, newNode);
+            addNode(newNode);
 
-    private void addHead(int key, int val) {
-        Node node = new Node(key, val);
-        Node next = dummyHead.next;         // next -> head
-        dummyHead.next = node;              // dummyHead -> node
-        node.next = next;                   // dummyHead -> node -> next
-        next.prev = node;                   // dummyHead -> node <-> next
-        node.prev = dummyHead;              // dummyHead <-> node <-> next
-        map.put(key, node);
-        size++;
-        if (size > capacity) {
-            // dummyHead 就用在这儿，尾部 node 表示最长时间没更新的 node
-            Node preTail = dummyTail.prev;
-            remove(preTail.key);
+            size++;
+            if (size > capacity) {
+                // pop the tail
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                size--;
+            }
         }
     }
 }
