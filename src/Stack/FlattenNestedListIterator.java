74440b99bdb0b04a12b9dfa5a60eb431072d695c
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-07-30 23:27:22 -0400

    7.30

--- src/Stack/FlattenNestedListIterator.java
+++ src/Stack/FlattenNestedListIterator.java
@@ -17,17 +17,80 @@ package Stack;
                  the order of elements returned by next should be: [1,4,6].
  */
 
-import Stack.NestedInteger;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 
+/*  Stack
+
+ */
 public class FlattenNestedListIterator implements Iterator<Integer>{
-    Stack<NestedInteger> stack;
+    private Stack<Integer> stack;
+
+    public FlattenNestedListIterator(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        helper(nestedList);
+    }
+
+    private void helper(List<NestedInteger> nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            if (nestedList.get(i).isInteger()) {
+                stack.push(nestedList.get(i).getInteger());
+            } else {
+                helper(nestedList.get(i).getList());
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return stack.isEmpty() ? -1 : stack.pop();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+/*  Queue
+    和上面 Stack 也是一样的效果
+
+    private Queue<Integer> queue;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        queue = new LinkedList<>();
+        helper(nestedList);
+    }
+
+    private void helper(List<NestedInteger> nestedList) {
+        for (NestedInteger ni : nestedList) {
+            if (ni.isInteger()) {
+                stack.push(ni.getInteger());
+            } else {
+                helper(ni.getList());
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return queue.isEmpty() ? -1 : queue.poll();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
+ */
+
+/*  复杂版
+
+    private Stack<NestedInteger> stack;
 
     public void NestedIterator(List<NestedInteger> nestedList)  {
         stack = new Stack<>();
+        // 从后往前 push 以实现正序
         for (int i = nestedList.size() - 1; i >= 0; i--) {
             stack.push(nestedList.get(i));
         }
@@ -52,4 +115,4 @@ public class FlattenNestedListIterator implements Iterator<Integer>{
     public Integer next() {
         return stack.pop().getInteger();
     }
-}
+ */
